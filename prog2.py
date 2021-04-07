a840ebfeb92bc61c45dfafdee16c2076b79b1599
Nimesh Srivastava <snimesh412@gmail.com>
2021-04-07 22:21:45 +0530

    Initial commit prog2

---
+++ prog2.py
@@ -0,0 +1,8 @@
+import numpy as np
+import matplotlib.pyplot as plt
+
+# We will use os to iterate through different directories and join paths, etc.
+import os
+
+# cv2 will be used to carry out image operations
+import cv2
